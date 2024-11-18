cda40f075baf09a0d72e97e1dcb87c166ba6d5dc
Junior Rantila <junior.rantila@gmail.com>
2024-11-19 00:43:17 +0100

    MusicStudio: Add build.def

--- MusicStudio/main.cpp
+++ MusicStudio/main.cpp
@@ -1,17 +1,33 @@
-#include "./Application.h"
-
-#include <FS/Bundle.h>
-#include <CLI/ArgumentParser.h>
-#include <Core/Print.h>
 #include <Main/Main.h>
+#include <Core/Print.h>
+#include <CLI/ArgumentParser.h>
+#include <SoundIo/SoundIo.h>
+#include <FS/Bundle.h>
 #include <Fonts/Fonts.h>
-#include <UI/Shaders/Shaders.h>
+#include <Shaders/Shaders.h>
+#include <Debug/Instrumentation.h>
+#include <Ty/ArenaAllocator.h>
+#include <MS/Plugin.h>
+#include <MS/Project.h>
+#include <MS/PluginManager.h>
+#include <UI/Application.h>
+#include <UI/Window.h>
+#include <Render/Render.h>
 
-ErrorOr<int> Main::main(int argc, c_string argv[])
+template <typename T>
+void swap(T* a, T* b)
+{
+    T c = *a;
+    T d = *b;
+    *a = d;
+    *b = c;
+}
+
+ErrorOr<int> Main::main(int argc, c_string *argv)
 {
     auto bundle = FS::Bundle()
         .add_pack(Fonts())
-        .add_pack(UI::Shaders());
+        .add_pack(Shaders());
 
     dbgln("resources:"sv);
     for (auto resource : bundle.resources()) {
@@ -25,9 +41,18 @@ ErrorOr<int> Main::main(int argc, c_string argv[])
         root_directory = StringView::from_c_string(arg);
     }));
 
-    auto plugin_paths = Vector<StringView>();
+    auto plugin_paths = Vector<c_string>();
     TRY(argument_parser.add_option("--plugin", "-p", "plugin", "open plugin", [&](c_string arg) {
-        MUST(plugin_paths.append(StringView::from_c_string(arg)));
+        MUST(plugin_paths.append(arg));
+    }));
+
+    auto hot_reload = ""sv;
+    TRY(argument_parser.add_option("--hot-reload", "-h", "shared-library", "hot reload", [&](c_string arg) {
+        hot_reload = StringView::from_c_string(arg);
+    }));
+
+    TRY(argument_parser.add_flag("--call-graph", "-c", "show function call graph", [&]() {
+        Debug::Instrumentation::enabled = true;
     }));
 
     if (auto result = argument_parser.run(argc, argv); result.is_error()) {
@@ -35,13 +60,68 @@ ErrorOr<int> Main::main(int argc, c_string argv[])
         return 1;
     }
 
-    auto application = TRY(Application::create(bundle));
+    auto* app = ui_application_create(0);
+    auto* window = ui_window_create(app, {
+        .parent = nullptr,
+        .title = "MusicStudio",
+        .x = 0,
+        .y = 0,
+        .width = 900,
+        .height = 600,
+    });
 
-    for (auto plugin : plugin_paths) {
-        TRY(application.open_plugin(plugin));
+    auto arena = TRY(ArenaAllocator::create(1024ULL * 1024ULL));
+    auto project = MS::Project();
+    auto manager = TRY(MS::PluginManager::create(&arena)).init(&project);
+    auto plugins = TRY(arena.alloc<Id<MS::Plugin>>(1024));
+
+    usize plugin_count = 0;
+    for (usize i = 0; i < plugin_paths.size(); i++) {
+        plugin_count += 1;
+        plugins[i] = TRY(manager.instantiate(plugin_paths[i]));
     }
-    TRY(application.change_path(root_directory));
+    (void)plugin_count;
+    for (usize i = 0; i < plugin_count; i++) {
+        manager.plugins[i].init();
+    }
 
-    application.run();
+    auto out_frames = TRY(arena.alloc<f32>(project.sample_rate * project.channels)).zero();
+    auto in_frames = TRY(arena.alloc<f32>(project.sample_rate * project.channels)).zero();
+    usize even = 0;
+    for (usize i = 0; i < plugin_count; i++) {
+        even = (even + 1) & 1;
+        manager.plugins[i].process_f32(out_frames.data(), in_frames.data(), project.sample_rate);
+        swap(&out_frames, &in_frames);
+    }
+    swap(&out_frames, &in_frames);
+    dbgln(out_frames[0]);
+
+    auto* render = render_create(&bundle, &arena);
+    if (!render) {
+        return Error::from_string_literal("could not create renderer: out of memory");
+    }
+    Defer destroy_render = [&] {
+        render_destroy(render);
+    };
+
+    while (!ui_window_should_close(window)) {
+        ui_application_poll_events(app);
+        ui_window_gl_make_current_context(window);
+
+        i32 width = 0;
+        i32 height = 0;
+        ui_window_size(window, &width, &height);
+        render_set_resolution(render, vec2f(width, height));
+        render_clear(render, {
+            .r = 0.1f,
+            .g = 0.2f,
+            .b = 0.2f,
+            .a = 1.0f,
+        });
+        render_flush(render);
+
+        ui_window_gl_flush(window);
+    }
+
     return 0;
 }

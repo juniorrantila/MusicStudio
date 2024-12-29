24ef2ae32c3322a526504f146f596e8ddda8e874
Junior Rantila <junior.rantila@gmail.com>
2024-12-29 08:34:23 +0100

    Libraries: Add NanoVG

---
+++ Libraries/NanoVG/NanoVGL.h
@@ -0,0 +1,4 @@
+#include <GL/GL.h>
+#include "./nanovg.h"
+#define NANOVG_GL3
+#include "./nanovg_gl.h"
---
+++ Libraries/NanoVG/nanovg_gl.c
@@ -0,0 +1,5 @@
+#include <GL/GL.h>
+#include "./nanovg.h"
+
+#define NANOVG_GL3_IMPLEMENTATION
+#include "./nanovg_gl.h"

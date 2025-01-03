815ba9eeb7745daf5685466ac586eb8bf9d05d1c
Junior Rantila <junior.rantila@gmail.com>
2025-01-03 05:19:36 +0100

    Libraries: Remove OldUI

--- Libraries/OldUI/Forward.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-namespace UI {
-
-struct UI;
-struct FreeGlyphAtlas;
-struct SimpleRenderer;
-
-}
--- Libraries/OldUI/Graphics/GL.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-#ifdef __APPLE__
-#include <OpenGL/gl3.h>
-#else
-#include <GL/glew.h>
-#endif

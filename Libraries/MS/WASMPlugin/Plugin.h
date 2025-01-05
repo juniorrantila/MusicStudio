4e94ae93a1f2c46a3bf96e93031fd6ac72ff038c
Junior Rantila <junior.rantila@gmail.com>
2025-01-05 07:43:59 +0100

    MS/WASMPlugin: Add more plugin meta information
    This includes plugin_version, plugin_id and plugin_kind.

--- Libraries/MS/WASMPlugin/Plugin.h
+++ Libraries/MS/WASMPlugin/Plugin.h
@@ -46,7 +46,24 @@ MS_WASM("ms", "heap_end")
 void* ms_heap_end(void);
 
 // Defined by the plugin.
-extern const char ms_plugin_name[] __asm__("ms_plugin_name");
+
+// This is used to categorize the plugin in the MusicStudio UI.
+typedef char const MSPluginKind[];
+#define MSPluginKind_Effect     ("com.music-studio.effect")
+#define MSPluginKind_Generator  ("com.music-studio.generator")
+#define MSPluginKind_Instrument ("com.music-studio.instrument")
+
+// Version needs to be different if a memcpy of memory
+// isn't sufficient for initializing a plugin between runs.
+// NOTE: May not be 0.
+extern u32 const ms_plugin_version __asm__("ms_plugin_version");
+extern char const ms_plugin_id[] __asm__("ms_plugin_id");
+extern char const ms_plugin_name[] __asm__("ms_plugin_name");
+extern MSPluginKind const ms_plugin_kind __asm__("ms_plugin_kind");
+
+void ms_plugin_init(u32 plugin_version) __asm__("ms_plugin_init");
+void ms_plugin_deinit(void) __asm__("ms_plugin_deinit");
+
 void ms_plugin_draw_ui(void) __asm__("ms_plugin_draw_ui");
 void ms_plugin_process_f64(f64* out, f64 const* in, u32 frames, u32 channels) __asm__("ms_plugin_process_f64");
 void ms_plugin_process_f32(f32* out, f32 const* in, u32 frames, u32 channels) __asm__("ms_plugin_process_f32");

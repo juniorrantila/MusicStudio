44b61eda4681e15d9a8280a0d3bf3cae1bdbbe37
Junior Rantila <junior.rantila@gmail.com>
2025-02-17 21:49:46 +0100

    Core: Ensure timespec is zero initialized
    In case clock_gettime() fails, we probably don't want garbage in our
    return value.

--- Libraries/Core/Time.cpp
+++ Libraries/Core/Time.cpp
@@ -6,7 +6,7 @@ namespace Core {
 
 f64 time(void)
 {
-    struct timespec spec;
+    struct timespec spec {};
     clock_gettime(CLOCK_REALTIME, &spec);
     return ((f64)spec.tv_sec) + (spec.tv_nsec / 1.0e9);
 }

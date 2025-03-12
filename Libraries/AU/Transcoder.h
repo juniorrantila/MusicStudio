90c6aeb66b3e53914dd9283705a5d670a73f65de
Junior Rantila <junior.rantila@gmail.com>
2025-03-12 22:36:42 +0100

    AU: Add functions for (de)interlacing audio

---
+++ Libraries/AU/Transcoder.h
@@ -0,0 +1,12 @@
+#pragma once
+#include <Ty/Base.h>
+#include <Ty/Allocator.h>
+
+C_API f64* au_interlace_f64(Allocator*, f64 const* in, usize frames, usize channels);
+C_API f64* au_interlace_f64_from_f32(Allocator*, f32 const* in, usize frames, usize channels);
+
+C_API f64* au_deinterlace_f64(Allocator*, f64 const* in, usize frames, usize channels);
+C_API f32* au_deinterlace_f32_from_f64(Allocator*, f64 const* in, usize frames, usize channels);
+
+C_API f64** au_shallow_split_channels_f64(Allocator* arena, usize min_splits, f64* in, usize frames, usize channels);
+C_API f32** au_shallow_split_channels_f32(Allocator* arena, usize min_splits, f32* in, usize frames, usize channels);

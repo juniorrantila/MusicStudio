4acd4e27ed62630e055fa446a5888754005173d2
Junior Rantila <junior.rantila@gmail.com>
2025-03-24 20:09:41 +0100

    Core: Cast nanoseconds to f64 before dividing

--- Libraries/Core/Time.cpp
+++ Libraries/Core/Time.cpp
@@ -7,8 +7,8 @@ namespace Core {
 f64 time(void)
 {
     struct timespec spec {};
-    clock_gettime(CLOCK_REALTIME, &spec);
-    return ((f64)spec.tv_sec) + (spec.tv_nsec / 1.0e9);
+    (void)clock_gettime(CLOCK_REALTIME, &spec);
+    return ((f64)spec.tv_sec) + (((f64)spec.tv_nsec) / 1.0e9);
 }
 
 }

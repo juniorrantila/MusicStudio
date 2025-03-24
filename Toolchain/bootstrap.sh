1191483db88d8d8360e0967a0e73e4a1eda00a40
Junior Rantila <junior.rantila@gmail.com>
2025-03-24 20:26:59 +0100

    Toolchain: Build tools only if they are not installed

--- Toolchain/bootstrap.sh
+++ Toolchain/bootstrap.sh
@@ -5,8 +5,8 @@ set -e
 BASE_PATH=`realpath $(dirname $0)`
 TOOLCHAIN_DIR="$BASE_PATH"
 
-"$TOOLCHAIN_DIR/build-python.sh"
-"$TOOLCHAIN_DIR/build-ninja.sh"
-"$TOOLCHAIN_DIR/build-cmake.sh"
-"$TOOLCHAIN_DIR/build-ccache.sh"
+which python > /dev/null || "$TOOLCHAIN_DIR/build-python.sh"
+which ninja > /dev/null || "$TOOLCHAIN_DIR/build-ninja.sh"
+which cmake > /dev/null || "$TOOLCHAIN_DIR/build-cmake.sh"
+which ccache > /dev/null || "$TOOLCHAIN_DIR/build-ccache.sh"
 "$TOOLCHAIN_DIR/build-llvm.sh"

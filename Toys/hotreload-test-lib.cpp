0dc4ad9dae081666d3b2f58d38afc97949994f78
Junior Rantila <junior.rantila@gmail.com>
2025-03-28 17:59:36 +0100

    Toys: Use actor model for hotreload-test

--- Toys/hotreload-test-lib.cpp
+++ Toys/hotreload-test-lib.cpp
@@ -1,32 +1,44 @@
 #include <Library/HotReload.h>
+#include <Ty/StringSlice.h>
+#include <Ty2/Logger.h>
+#include <FS/FSVolume.h>
 
 #include <math.h>
 
-C_API f64 square(f64 time);
-C_API f64 square(f64 time)
+struct Context {
+    Allocator* arena;
+    Logger* log;
+    FSVolume* volume;
+};
+
+struct AudioContext {
+    Context* host;
+};
+
+static f64 square(f64 time)
 {
     time = fmod(time, 1.0);
     if (time < 0.5) return -1.0;
     return 1.0;
 }
 
-C_API f64 min(f64 a, f64 b);
-C_API f64 min(f64 a, f64 b)
+static f64 min(f64 a, f64 b)
 {
     if (a < b) return a;
     return b;
 }
 
-C_API f64 max(f64 a, f64 b);
-C_API f64 max(f64 a, f64 b)
+static f64 max(f64 a, f64 b)
 {
     if (a > b) return a;
     return b;
 }
 
-C_API f64 sample_at_time(void*, f64 time);
-C_API f64 sample_at_time(void*, f64 time)
+static f64 sample_at_time(void* ctx, f64 time)
 {
+    auto* audio = (AudioContext*)ctx;
+    (void)audio;
+
     time *= 0.4;
     (void)min;
     f64 m = sin(time * 2 * M_PI * 100) * 0.5;
@@ -41,13 +53,35 @@ C_API f64 sample_at_time(void*, f64 time)
     return max(a, b);
 }
 
+static void init(void* state, void const* arg, usize size)
+{
+    VERIFY(size == sizeof(Context));
+    auto* audio = (AudioContext*)state;
+    audio->host = (Context*)arg;
+}
+
+static void deinit(void*)
+{
+}
+
+static void* find(StringSlice symbol_name)
+{
+    if (symbol_name.equal("sample_at_time"s))
+        return (void*)sample_at_time;
+    if (symbol_name.equal("init"s))
+        return (void*)init;
+    if (symbol_name.equal("deinit"s))
+        return (void*)deinit;
+    return 0;
+}
+
 C_API void* hotreload_dispatch(HotReloadEvent event);
 C_API void* hotreload_dispatch(HotReloadEvent event)
 {
     switch (event.tag) {
     case HotReloadTag_Size:
-    case HotReloadTag_Init:
-    case HotReloadTag_Deinit:
-        return 0;
+        return (void*)sizeof(AudioContext);
+    case HotReloadTag_Find:
+        return find(string_slice((char*)event.state, event.size));
     }
 }
--- Toys/hotreload-test.cpp
+++ Toys/hotreload-test.cpp
@@ -7,19 +7,22 @@
 #include <Ty2/FileLogger.h>
 #include <FS/OS.h>
 #include <SoundIo/SoundIo.h>
+#include <Library/Actor.h>
+#include <Ty/Defer.h>
 
 #include <math.h>
 #include <setjmp.h>
 #include <unistd.h>
 #include <signal.h>
 
-struct HotLib {
-    void* state;
-    f64 (*sample_at_time)(void*, f64 time);
+struct AudioActorModel;
+struct Audio {
+    AudioActorModel* model;
+    f64 (*sample_at_time)(AudioActorModel*, f64 time);
 };
 
 struct AudioContext {
-    HotLib hotlib;
+    Audio audio;
     f64 seconds_offset;
 };
 
@@ -30,8 +33,8 @@ struct Context {
 };
 
 static SoundIoOutStream* create_default_outstream(SoundIo*);
-static Library* hotlib_create(Context const*);
-static HotLib hotlib_mount(Library const*);
+static Actor* audio_actor_create(Context const*);
+static Audio audio_actor_mount(Actor const*);
 
 ErrorOr<int> Main::main(int, c_string[])
 {
@@ -51,11 +54,11 @@ ErrorOr<int> Main::main(int, c_string[])
         .log = log,
         .volume = volume,
     };
-    auto* lib = hotlib_create(&ctx);
-    if (!lib) log->fatal("could not create hotlib");
+    Actor* audio_actor = audio_actor_create(&ctx);
+    if (!audio_actor) log->fatal("could not create audio actor");
 
     auto audio_context = AudioContext{
-        .hotlib = hotlib_mount(lib),
+        .audio = audio_actor_mount(audio_actor),
         .seconds_offset = 0.0,
     };
 
@@ -80,23 +83,32 @@ ErrorOr<int> Main::main(int, c_string[])
             .tv_nsec = 1000000,
         };
         fs_volume_poll_events(volume, &timeout);
-        library_update(lib);
-        if (library_needs_reload(lib)) {
+        actor_update(audio_actor);
+        if (actor_needs_reload(audio_actor)) {
             soundio_outstream_pause(outstream, true);
-            bool success = library_reload(lib);
-            audio_context.hotlib = hotlib_mount(lib);
+            bool success = actor_reload(audio_actor);
+            audio_context.audio = audio_actor_mount(audio_actor);
             soundio_outstream_pause(outstream, false);
-            if (success) log->info("reloaded audio library");
-            else log->warning("could not reload audio library");
+            if (success) log->info("reloaded audio actor");
+            else log->warning("could not reload audio actor");
         }
     }
 
     return 0;
 }
 
-static Library* hotlib_create(Context const* ctx)
+static Actor* audio_actor_create(Context const* ctx)
 {
     auto path = HOTRELOAD_LIB_DIR "/hotreload-test-lib.hotlib"s;
+
+    auto* actor = (Actor*)memalloc(ctx->arena, sizeof(Actor), alignof(Actor));
+    if (!actor) {
+        ctx->log->error("could not allocate audio actor");
+        return nullptr;
+    }
+    Defer free_actor = [&]{
+        memfree(ctx->arena, actor, sizeof(Actor), alignof(Actor));
+    };
 
     FSFile file;
     if (!fs_system_open(ctx->arena, path, &file)) {
@@ -116,15 +128,27 @@ static Library* hotlib_create(Context const* ctx)
         ctx->log->error("could not load '%.*s': %s", (int)path.count, path.items, library_strerror(error));
         return nullptr;
     }
+    Defer destroy_lib = [&]{
+        library_destroy(lib);
+    };
 
-    return lib;
+    if (!actor_create_from_library(ctx->arena, lib, actor)) {
+        ctx->log->error("could not create actor from library");
+        return nullptr;
+    }
+
+    actor_init(actor, ctx, sizeof(*ctx));
+
+    destroy_lib.disarm();
+    free_actor.disarm();
+    return actor;
 }
 
-HotLib hotlib_mount(Library const* lib)
+Audio audio_actor_mount(Actor const* actor)
 {
-    return (HotLib){
-        .state = library_hotreload_state(lib),
-        .sample_at_time = (f64(*)(void*, f64))library_get_symbol(lib, "sample_at_time"),
+    return (Audio){
+        .model = (AudioActorModel*)actor->model,
+        .sample_at_time = (f64(*)(AudioActorModel*, f64))actor_find_symbol(actor, "sample_at_time"s),
     };
 }
 
@@ -144,7 +168,7 @@ static void write_callback(SoundIoOutStream* outstream, int, int frame_count_max
     f64 seconds_per_frame = 1.0 / float_sample_rate;
     SoundIoChannelArea* areas = nullptr;
     auto* ctx = (AudioContext*)outstream->userdata;
-    auto* hotlib = &ctx->hotlib;
+    auto* audio = &ctx->audio;
 
     f64 seconds_offset = ctx->seconds_offset;
     int frames_left = frame_count_max;
@@ -161,8 +185,8 @@ static void write_callback(SoundIoOutStream* outstream, int, int frame_count_max
         auto* layout = &outstream->layout;
         for (int frame = 0; frame < frame_count; frame += 1) {
             f64 sample = 0.0;
-            if (hotlib->sample_at_time) {
-                sample = hotlib->sample_at_time(hotlib->state, seconds_offset + frame * seconds_per_frame);
+            if (audio->sample_at_time) {
+                sample = audio->sample_at_time(audio->model, seconds_offset + frame * seconds_per_frame);
             }
             for (int channel = 0; channel < layout->channel_count; channel += 1) {
                 *(f64*)areas[channel].ptr = sample;

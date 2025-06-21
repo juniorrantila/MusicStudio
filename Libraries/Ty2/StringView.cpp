53b02278cb118226a77a480343c3e2d58899f105
Junior Rantila <junior.rantila@gmail.com>
2025-06-21 20:44:50 +0200

    Ty2: Rename StringView => StringView2
    This is just to avoid name collision with Ty::StringView

--- Libraries/Ty2/StringView.cpp
+++ Libraries/Ty2/StringView.cpp
@@ -4,30 +4,30 @@
 
 #include <string.h>
 
-StringView StringView::from_parts(char const* s, usize count) { return sv_from_parts(s, count); }
-C_API StringView sv_from_parts(char const* s, usize count)
+StringView2 StringView2::from_parts(char const* s, usize count) { return sv_from_parts(s, count); }
+C_API StringView2 sv_from_parts(char const* s, usize count)
 {
-    return (StringView){
+    return (StringView2){
         .items = s,
         .count = count,
     };
 }
 
-StringView StringView::from_c_string(c_string s) { return sv_from_c_string(s); }
-C_API StringView sv_from_c_string(c_string s)
+StringView2 StringView2::from_c_string(c_string s) { return sv_from_c_string(s); }
+C_API StringView2 sv_from_c_string(c_string s)
 {
     if (!s) return sv_empty();
     return sv_from_parts(s, strlen(s));
 }
 
-StringView StringView::empty() { return sv_empty(); }
-C_API StringView sv_empty(void)
+StringView2 StringView2::empty() { return sv_empty(); }
+C_API StringView2 sv_empty(void)
 {
     return sv_from_parts(0, 0);
 }
 
-StringView StringView::clone(Allocator* a) const { return sv_clone(a, *this); }
-C_API StringView sv_clone(Allocator* a, StringView s)
+StringView2 StringView2::clone(Allocator* a) const { return sv_clone(a, *this); }
+C_API StringView2 sv_clone(Allocator* a, StringView2 s)
 {
     if (sv_is_empty(s)) return sv_empty();
     char const* buf = (char const*)memclone(a, s.items, s.count, 1);
@@ -35,16 +35,16 @@ C_API StringView sv_clone(Allocator* a, StringView s)
     return sv_from_parts(buf, s.count);
 }
 
-bool StringView::is_empty() const { return sv_is_empty(*this); }
-C_API bool sv_is_empty(StringView s)
+bool StringView2::is_empty() const { return sv_is_empty(*this); }
+C_API bool sv_is_empty(StringView2 s)
 {
     if (s.items == nullptr) return true;
     if (s.count == 0) return true;
     return false;
 }
 
-bool StringView::equal(StringView other) const { return sv_equal(*this, other); }
-C_API bool sv_equal(StringView a, StringView b)
+bool StringView2::equal(StringView2 other) const { return sv_equal(*this, other); }
+C_API bool sv_equal(StringView2 a, StringView2 b)
 {
     if (a.count != b.count) return false;
     if (a.items == b.items) return true;
@@ -53,24 +53,24 @@ C_API bool sv_equal(StringView a, StringView b)
     return memcmp(a.items + 1, b.items + 1, a.count - 1) == 0;
 }
 
-StringView StringView::slice(usize start, usize size) const { return sv_slice(*this, start, size); }
-C_API StringView sv_slice(StringView s, usize start, usize size)
+StringView2 StringView2::slice(usize start, usize size) const { return sv_slice(*this, start, size); }
+C_API StringView2 sv_slice(StringView2 s, usize start, usize size)
 {
     if (sv_is_empty(s)) return sv_from_parts(s.items, 0);
     if (s.count - start < size) size = s.count - start;
     return sv_from_parts(s.items + start, size);
 }
 
-StringView StringView::part(usize start, usize end) const { return sv_part(*this, start, end); }
-C_API StringView sv_part(StringView s, usize start, usize end)
+StringView2 StringView2::part(usize start, usize end) const { return sv_part(*this, start, end); }
+C_API StringView2 sv_part(StringView2 s, usize start, usize end)
 {
     VERIFY(end >= start);
     if (sv_is_empty(s)) return sv_from_parts(s.items, 0);
     return sv_slice(s, start, end - start);
 }
 
-StringView StringView::chop_left(usize count) const { return sv_chop_left(*this, count); }
-StringView sv_chop_left(StringView s, usize count)
+StringView2 StringView2::chop_left(usize count) const { return sv_chop_left(*this, count); }
+StringView2 sv_chop_left(StringView2 s, usize count)
 {
     return sv_part(s, count, s.count);
 }
--- Libraries/Ty2/StringView.h
+++ Libraries/Ty2/StringView.h
@@ -3,37 +3,37 @@
 
 #include "./Allocator.h"
 
-typedef struct StringView {
+typedef struct StringView2 {
     char const* items;
     usize count;
 
 #ifdef __cplusplus
-    static StringView from_parts(char const*, usize);
-    static StringView from_c_string(c_string);
-    static StringView empty();
+    static StringView2 from_parts(char const*, usize);
+    static StringView2 from_c_string(c_string);
+    static StringView2 empty();
 
-    StringView clone(Allocator*) const;
+    StringView2 clone(Allocator*) const;
 
     bool is_empty() const;
 
-    bool equal(StringView other) const;
-    StringView slice(usize start, usize size) const;
-    StringView part(usize start, usize end) const;
+    bool equal(StringView2 other) const;
+    StringView2 slice(usize start, usize size) const;
+    StringView2 part(usize start, usize end) const;
 
-    StringView chop_left(usize count) const;
+    StringView2 chop_left(usize count) const;
 #endif
-} StringView;
+} StringView2;
 
-C_API StringView sv_from_parts(char const*, usize);
-C_API StringView sv_from_c_string(c_string);
-C_API StringView sv_clone(Allocator*, StringView);
+C_API StringView2 sv_from_parts(char const*, usize);
+C_API StringView2 sv_from_c_string(c_string);
+C_API StringView2 sv_clone(Allocator*, StringView2);
 
-C_API bool sv_is_empty(StringView s);
-C_API StringView sv_empty(void);
+C_API bool sv_is_empty(StringView2 s);
+C_API StringView2 sv_empty(void);
 
-C_API bool sv_equal(StringView, StringView);
+C_API bool sv_equal(StringView2, StringView2);
 
-C_API StringView sv_slice(StringView, usize start, usize size);
-C_API StringView sv_part(StringView, usize start, usize end);
+C_API StringView2 sv_slice(StringView2, usize start, usize size);
+C_API StringView2 sv_part(StringView2, usize start, usize end);
 
-C_API StringView sv_chop_left(StringView, usize count);
+C_API StringView2 sv_chop_left(StringView2, usize count);

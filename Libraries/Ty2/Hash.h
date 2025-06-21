aae6253b59a196a8302e44fb8a0777d48d596850
Junior Rantila <junior.rantila@gmail.com>
2025-06-21 17:56:46 +0200

    Ty2: Use types with explicit size in hash functions

--- Libraries/Ty2/Hash.h
+++ Libraries/Ty2/Hash.h
@@ -1,32 +1,41 @@
 #pragma once
 #include "./Base.h"
 
-static const usize djb2_initial_seed = 5381;
-C_API inline usize djb2(usize seed, void const* value, usize size)
+static const u64 djb2_initial_seed = 5381;
+C_INLINE u64 djb2(u64 seed, void const* value, u64 size)
 {
     u8 const* data = (u8 const*)value;
-    usize hash = seed;
+    u64 hash = seed;
     int c = 0;
 
-    for (usize i = 0; i < size; i++) {
+    for (u64 i = 0; i < size; i++) {
         c = data[i];
         hash = ((hash << 5) + hash) + c; // hash * 33 + c
     }
 
     return hash;
 }
+C_INLINE u64 djb2_u64(u64 seed, u64 value)
+{
+    return djb2(seed, &value, sizeof(value));
+}
 
-static const usize sdbm_initial_seed = 5381;
-C_API inline usize sdbm(usize seed, void const* value, usize size)
+static const u64 sdbm_initial_seed = 5381;
+C_INLINE u64 sdbm(u64 seed, void const* value, u64 size)
 {
     u8 const* data = (u8 const*)value;
-    usize hash = seed;
+    u64 hash = seed;
     int c = 0;
 
-    for (usize i = 0; i < size; i++) {
+    for (u64 i = 0; i < size; i++) {
         c = data[i];
         hash = c + (hash << 6) + (hash << 16) - hash;
     }
 
     return hash;
+}
+
+C_INLINE u64 sdbm_u64(u64 seed, u64 value)
+{
+    return sdbm(seed, &value, sizeof(value));
 }

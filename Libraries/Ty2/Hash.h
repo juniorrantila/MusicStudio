4a4ac419cfbb47c2c225b69d130ea5dca8ef8780
Junior Rantila <junior.rantila@gmail.com>
2025-07-03 22:24:43 +0200

    Ty2: Disable overflow check of hash functions

--- Libraries/Ty2/Hash.h
+++ Libraries/Ty2/Hash.h
@@ -1,7 +1,11 @@
 #pragma once
 #include "./Base.h"
 
+#define NO_OVERFLOW_CHECK __attribute__((no_sanitize("unsigned-integer-overflow")))
+
 static const u64 djb2_initial_seed = 5381;
+
+NO_OVERFLOW_CHECK
 C_INLINE u64 djb2(u64 seed, void const* value, u64 size)
 {
     u8 const* data = (u8 const*)value;
@@ -10,17 +14,20 @@ C_INLINE u64 djb2(u64 seed, void const* value, u64 size)
 
     for (u64 i = 0; i < size; i++) {
         c = data[i];
-        hash = ((hash << 5) + hash) + c; // hash * 33 + c
+        hash = (hash * 33 + c);
     }
 
     return hash;
 }
+
+NO_OVERFLOW_CHECK
 C_INLINE u64 djb2_u64(u64 seed, u64 value)
 {
     return djb2(seed, &value, sizeof(value));
 }
 
 static const u64 sdbm_initial_seed = 5381;
+NO_OVERFLOW_CHECK
 C_INLINE u64 sdbm(u64 seed, void const* value, u64 size)
 {
     u8 const* data = (u8 const*)value;
@@ -35,6 +42,7 @@ C_INLINE u64 sdbm(u64 seed, void const* value, u64 size)
     return hash;
 }
 
+NO_OVERFLOW_CHECK
 C_INLINE u64 sdbm_u64(u64 seed, u64 value)
 {
     return sdbm(seed, &value, sizeof(value));

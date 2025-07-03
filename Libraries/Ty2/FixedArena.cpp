83fb8c2bc5133ac615c9928e5ab2bbaa09d3eddc
Junior Rantila <junior.rantila@gmail.com>
2025-07-03 22:19:11 +0200

    Ty2: Use explicit sizes

--- Libraries/Ty2/FixedArena.cpp
+++ Libraries/Ty2/FixedArena.cpp
@@ -5,13 +5,13 @@
 #include "./Verify.h"
 
 static void* dispatch(Allocator*, AllocatorEvent);
+static bool owns(FixedArena const* arena, void* ptr);
 
-FixedArena FixedArena::from_slice(void* memory, usize size) { return fixed_arena_from_slice(memory, size); }
-C_API FixedArena fixed_arena_from_slice(void* memory, usize size)
+C_API FixedArena fixed_arena_init(void* memory, u64 size)
 {
     VERIFY(memory != nullptr);
     VERIFY(size != 0);
-    memset_canary(memory, size);
+    mempoison(memory, size);
     return {
         .allocator = make_allocator(dispatch),
         .base = (u8*)memory,
@@ -20,8 +20,8 @@ C_API FixedArena fixed_arena_from_slice(void* memory, usize size)
     };
 }
 
-usize FixedArena::bytes_used() const { return fixed_arena_bytes_used(this); }
-C_API usize fixed_arena_bytes_used(FixedArena const* arena)
+u64 FixedArena::bytes_used() const { return fixed_arena_bytes_used(this); }
+C_API u64 fixed_arena_bytes_used(FixedArena const* arena)
 {
     return arena->head - arena->base;
 }
@@ -30,9 +30,10 @@ void FixedArena::drain() { return fixed_arena_drain(this); }
 C_API void fixed_arena_drain(FixedArena* arena)
 {
     VERIFY(arena->head >= arena->base);
-    uptr size = arena->head - arena->base;
+    VERIFY(arena->end >= arena->base);
+    u64 size = ((u64)arena->end) - ((u64)arena->base);
     arena->head = arena->base;
-    if (arena->head) memset_canary(arena->head, size);
+    mempoison(arena->head, size);
 }
 
 FixedMark FixedArena::mark() const { return fixed_arena_mark(this); }
@@ -44,61 +45,57 @@ C_API FixedMark fixed_arena_mark(FixedArena const* arena)
 void FixedArena::sweep(FixedMark mark) { return fixed_arena_sweep(this, mark); }
 C_API void fixed_arena_sweep(FixedArena* arena, FixedMark mark)
 {
-    VERIFY(arena->owns(mark.value));
+    VERIFY(owns(arena, mark.value));
     VERIFY(arena->head >= mark.value);
-    uptr size = mark.value - arena->head;
-    if (mark.value) memset_canary(mark.value, size);
+    VERIFY(mark.value != nullptr);
+    uptr size = arena->head - mark.value;
     arena->head = mark.value;
+    mempoison(arena->head, size);
 }
 
-void* FixedArena::alloc(usize size, usize align) { return fixed_arena_alloc(this, size, align); }
-C_API void* fixed_arena_alloc(FixedArena* arena, usize size, usize align)
+void* FixedArena::push(u64 size, u64 align) { return fixed_arena_push(this, size, align); }
+C_API void* fixed_arena_push(FixedArena* arena, u64 size, u64 align)
 {
     u8* new_head = __builtin_align_up(arena->head, align);
-    usize size_until_aligned = new_head - arena->head;
-    if ((new_head + size) > arena->end) {
+    if ((new_head + size) > arena->end)
         return nullptr;
-    }
-    memcheck_canary(arena->head, size_until_aligned);
-    memcheck_canary(new_head, size);
     arena->head = new_head;
     VERIFY(((uptr)arena->head) % align == 0);
     void* ptr = arena->head;
     arena->head += size;
+    memunpoison(ptr, size);
     return ptr;
 }
 
-void FixedArena::free(void* ptr, usize size, usize align) { return fixed_arena_free(this, ptr, size, align); }
-C_API void fixed_arena_free(FixedArena* arena, void* ptr, usize size, usize align)
-{
-    (void)align;
-    VERIFYS(ptr != nullptr, "trying to free null pointer");
-    VERIFY(arena->owns(ptr));
-    VERIFY(arena->base <= (arena->head - size));
-    if (arena->head - size == ptr) {
-        arena->head -= size;
-        memset_canary(ptr, size);
-    }
-}
-
-bool FixedArena::owns(void* ptr) const { return fixed_arena_owns(this, ptr); }
-bool fixed_arena_owns(FixedArena const* arena, void* ptr)
+static bool owns(FixedArena const* arena, void* ptr)
 {
     u8* m = (u8*)ptr;
     return m <= arena->end && m >= arena->base;
 }
+
+static void fixed_arena_free(FixedArena* arena, void* ptr, u64 size, u64 align)
+{
+    (void)align;
+    VERIFYS(ptr != nullptr, "trying to free null pointer");
+    VERIFY(owns(arena, ptr));
+    VERIFY(arena->base <= (arena->head - size));
+    mempoison(ptr, size);
+    if (arena->head - size == ptr)
+        arena->head -= size;
+}
+
 
 static void* dispatch(Allocator* a, AllocatorEvent event)
 {
     FixedArena* arena = field_base(FixedArena, allocator, a);
     switch (event.tag) {
     case AllocatorEventTag_Alloc:
-        return arena->alloc(event.byte_count, event.align);
+        return arena->push(event.byte_count, event.align);
     case AllocatorEventTag_Free:
-        arena->free(event.ptr, event.byte_count, event.align);
+        fixed_arena_free(arena, event.ptr, event.byte_count, event.align);
         return nullptr;
     case AllocatorEventTag_Owns:
-        return arena->owns(event.ptr) ? (void*)1 : 0;
+        return owns(arena, event.ptr) ? (void*)1 : 0;
     }
     return nullptr;
 }
--- Libraries/Ty2/FixedArena.h
+++ Libraries/Ty2/FixedArena.h
@@ -11,30 +11,24 @@ typedef struct FixedArena {
     u8* end;
 
 #ifdef __cplusplus
-    static FixedArena from_slice(void* memory, usize size);
-    usize bytes_used() const;
+    u64 bytes_used() const;
 
     void drain();
 
     FixedMark mark() const;
     void sweep(FixedMark mark);
 
-    bool owns(void* ptr) const;
-
-    void* alloc(usize size, usize align);
-    void free(void* ptr, usize size, usize align);
+    void* push(u64 size, u64 align);
 #endif
 } FixedArena;
 
-C_API FixedArena fixed_arena_from_slice(void* memory, usize size);
+C_API FixedArena fixed_arena_init(void* memory, u64 size);
+C_API [[nodiscard]] bool fixed_arena_init_with_capacity(Allocator*, u64 size, u64 align, FixedArena*);
 
-C_API usize fixed_arena_bytes_used(FixedArena const*);
+C_API u64 fixed_arena_bytes_used(FixedArena const*);
 C_API void fixed_arena_drain(FixedArena*);
 
 C_API FixedMark fixed_arena_mark(FixedArena const*);
 C_API void fixed_arena_sweep(FixedArena*, FixedMark mark);
 
-C_API bool fixed_arena_owns(FixedArena const*, void* ptr);
-
-C_API void* fixed_arena_alloc(FixedArena*, usize size, usize align);
-C_API void fixed_arena_free(FixedArena*, void* ptr, usize size, usize align);
+C_API void* fixed_arena_push(FixedArena*, u64 size, u64 align);

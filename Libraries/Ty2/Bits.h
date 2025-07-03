66be08c1b4400a688992158bc1e7d323ec8a4167
Junior Rantila <junior.rantila@gmail.com>
2025-07-03 22:24:05 +0200

    Ty2: Add endian conversion functions

--- Libraries/Ty2/Bits.h
+++ Libraries/Ty2/Bits.h
@@ -6,6 +6,72 @@ static constexpr u64 MiB = 1024 * KiB;
 static constexpr u64 GiB = 1024 * KiB;
 static constexpr u64 TiB = 1024 * GiB;
 
+C_INLINE u16 ty_device_endian_from_u16le(u16 value)
+{
+#if __BYTE_ORDER__ == __ORDER_LITTLE_ENDIAN__
+    return value;
+#elif __BYTE_ORDER__ == __ORDER_BIG_ENDIAN__
+    return __builtin_bswap16(value);
+#else
+#error "unknown endianness"
+#endif
+}
+
+C_INLINE u32 ty_device_endian_from_u32le(u32 value)
+{
+#if __BYTE_ORDER__ == __ORDER_LITTLE_ENDIAN__
+    return value;
+#elif __BYTE_ORDER__ == __ORDER_BIG_ENDIAN__
+    return __builtin_bswap32(value);
+#else
+#error "unknown endianness"
+#endif
+}
+
+C_INLINE u64 ty_device_endian_from_u64le(u64 value)
+{
+#if __BYTE_ORDER__ == __ORDER_LITTLE_ENDIAN__
+    return value;
+#elif __BYTE_ORDER__ == __ORDER_BIG_ENDIAN__
+    return __builtin_bswap64(value);
+#else
+#error "unknown endianness"
+#endif
+}
+
+C_INLINE u16 ty_device_endian_from_u16be(u16 value)
+{
+#if __BYTE_ORDER__ == __ORDER_LITTLE_ENDIAN__
+    return __builtin_bswap16(value);
+#elif __BYTE_ORDER__ == __ORDER_BIG_ENDIAN__
+    return value;
+#else
+#error "unknown endianness"
+#endif
+}
+
+C_INLINE u32 ty_device_endian_from_u32be(u32 value)
+{
+#if __BYTE_ORDER__ == __ORDER_LITTLE_ENDIAN__
+    return __builtin_bswap32(value);
+#elif __BYTE_ORDER__ == __ORDER_BIG_ENDIAN__
+    return value;
+#else
+#error "unknown endianness"
+#endif
+}
+
+C_INLINE u64 ty_device_endian_from_u64be(u64 value)
+{
+#if __BYTE_ORDER__ == __ORDER_LITTLE_ENDIAN__
+    return __builtin_bswap64(value);
+#elif __BYTE_ORDER__ == __ORDER_BIG_ENDIAN__
+    return value;
+#else
+#error "unknown endianness"
+#endif
+}
+
 #ifdef __cplusplus
 
 template <typename T, typename U>

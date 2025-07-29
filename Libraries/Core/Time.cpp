8d4ee55ae4c43cec1efef3513d9215cb34273bf4
Junior Rantila <junior.rantila@gmail.com>
2025-07-29 21:00:34 +0200

    Core: Ensure that core_time_since_start() never returns 0
    Calling core_time_now() in quick succession might yield a difference of
    zero, which other parts of the code might assume means "uninitialized",
    so returning epsilon instead.
    
    This fixes a bug where the first changed file in a FSVolume might not
    have been marked as stale.

--- Libraries/Core/Time.cpp
+++ Libraries/Core/Time.cpp
@@ -1,5 +1,6 @@
 #include "./Time.h"
 
+#include <float.h>
 #include <time.h>
 
 namespace Core {
@@ -24,5 +25,13 @@ C_API f64 core_time_since_start()
     if (start == 0.0) {
         start = core_time_now();
     }
-    return core_time_now() - start;
+    f64 now = core_time_now();
+    f64 time_since_start = now - start;
+
+    // Returning epsilon here since calling core_time_now() twice might yield
+    // a difference of zero, which is best to avoid for parts of the code base
+    // which assume that a value of zero means uninitialized.
+    if (time_since_start < DBL_EPSILON)
+        return DBL_EPSILON;
+    return time_since_start;
 }

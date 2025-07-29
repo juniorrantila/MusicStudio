66d083903efa193c11cce1227f8d5470d22a167b
Junior Rantila <junior.rantila@gmail.com>
2025-07-29 21:24:37 +0200

    Library: Rework Actor to not store the actor state
    Upon using this API, I realized that most (all) of the time we were only
    using one actor function and the state was mostly (all of the time)
    managed by the application itself.
    
    This patch simplifies Actor to not manage the actor state and removes
    the multiple use of multiple actor functions to just one.

--- Libraries/Library/Actor.c
+++ Libraries/Library/Actor.c
@@ -1,5 +1,6 @@
 #include "./Actor.h"
 
+#include <Ty2/Base.h>
 #include <FS/FSVolume.h>
 #include <Ty/Verify.h>
 
@@ -7,56 +8,19 @@
 #include <dlfcn.h>
 
 static bool is_library(Actor const* actor);
-static usize size(void*(*dispatch)(ActorEvent));
-static usize align(void*(*dispatch)(ActorEvent));
-static bool init(void*(*dispatch)(ActorEvent), u8* state, void const* arg, usize arg_size);
-static void deinit(void*(*dispatch)(ActorEvent), u8* state);
-static void bind(void*(*dispatch)(ActorEvent), u8* state);
 
-C_API bool actor_init(Logger* debug, void* (*dispatch)(ActorEvent), void const* arg, u64 arg_size, Actor* out)
+C_API Actor actor_init(void (*dispatch)(void))
 {
-    VERIFY(dispatch);
-    VERIFY(out);
-
-    usize state_size = size(dispatch);
-    if (state_size > actor_max_state_size) {
-        log_error_if(debug, "state size larger than allowed (size: %zu, max: %zu)", state_size, actor_max_state_size);
-        return false;
-    }
-    usize state_align = align(dispatch);
-    if (state_align > actor_max_state_align) {
-        log_error_if(debug, "state align larger than allowed (align: %zu, max: %zu)", state_align, actor_max_state_align);
-        return false;
-    }
-
-    *out = (Actor){
-        .current_state = out->state[0],
-        .state_index = 0,
-        .library = {
-            .volume = 0,
-            .dispatch_symbol = 0,
-            .file = {},
-            .needs_reload = false,
-            .handles = {},
-        },
-        .debug = debug,
+    return (Actor){
         .dispatch = dispatch,
-        .state = {},
     };
-    if (!init(dispatch, out->current_state, arg, arg_size)) {
-        log_error_if(debug, "running dispatch init failed");
-        memset(out, 0, sizeof(*out));
-        return false;
-    }
-    bind(dispatch, out->current_state);
-    return true;
 }
 
-C_API bool actor_init_reloadable(Logger* debug, FSVolume* volume, FileID library, c_string dispatch_name, void const* arg, u64 arg_size, Actor* out)
+C_API bool actor_init_reloadable(Actor* actor, Logger* debug, FSVolume* volume, FileID library, c_string dispatch_name)
 {
+    VERIFY(actor);
     VERIFY(volume);
     VERIFY(dispatch_name);
-    VERIFY(out);
 
     FSFile const* file = fs_volume_use_ref(volume, library);
     VERIFY(file->kind == FSFileMount_SystemMount);
@@ -69,56 +33,31 @@ C_API bool actor_init_reloadable(Logger* debug, FSVolume* volume, FileID library
         return false;
     }
 
-    void* (*dispatch)(ActorEvent) = dlsym(handle, dispatch_name);
+    void(*dispatch)(void) = dlsym(handle, dispatch_name);
     if (!dispatch) {
         log_error_if(debug, "could not find '%s'", dispatch_name);
         return false;
     }
 
-    usize state_size = size(dispatch);
-    if (state_size > actor_max_state_size) {
-        log_error_if(debug, "state size was bigger than allowed (size: %zu, max: %zu)", state_size, actor_max_state_size);
-        dlclose(handle);
-        return false;
-    }
-    usize state_align = align(dispatch);
-    if (state_align > actor_max_state_align) {
-        log_error_if(debug, "state align was bigger than allowed (align: %zu, max: %zu)", state_align, actor_max_state_align);
-        dlclose(handle);
-        return false;
-    }
-
-    *out = (Actor){
-        .current_state = out->state[0],
-        .state_index = 0,
-        .library = {
-            .volume = volume,
-            .dispatch_symbol = dispatch_name,
-            .file = library,
-            .needs_reload = false,
-            .handles = {
-                handle,
-            },
-        },
+    *actor = (Actor){
         .debug = debug,
+        .volume = volume,
+        .handles = {
+            handle,
+        },
+        .dispatch_name = dispatch_name,
         .dispatch = dispatch,
-        .state = {},
+        .current_handle = 0,
+        .needs_reload = false,
+        .library = library,
     };
-    memset(out->current_state, 0, state_size);
-    if (!init(dispatch, out->current_state, arg, arg_size)) {
-        log_error_if(debug, "running dispatch init failed");
-        memset(out, 0, sizeof(*out));
-        return false;
-    }
-    bind(dispatch, out->current_state);
     return true;
 }
 
 C_API void actor_deinit(Actor* actor)
 {
-    deinit(actor->dispatch, actor->current_state);
-    for (usize i = 0; i < actor_max_state_size; i++) {
-        void* handle = actor->library.handles[i];
+    for (usize i = 0; i < ty_array_size(actor->handles); i++) {
+        void* handle = actor->handles[i];
         if (handle) dlclose(handle);
     }
     memset(actor, 0, sizeof(*actor));
@@ -127,15 +66,15 @@ C_API void actor_deinit(Actor* actor)
 C_API void actor_update(Actor* actor)
 {
     if (!is_library(actor)) return;
-    if (fs_file_needs_reload(fs_volume_use_ref(actor->library.volume, actor->library.file))) {
-        actor->library.needs_reload = true;
+    if (fs_file_needs_reload(fs_volume_use_ref(actor->volume, actor->library))) {
+        actor->needs_reload = true;
     }
 }
 
 C_API bool actor_needs_reload(Actor const* actor)
 {
     if (!is_library(actor)) return false;
-    return actor->library.needs_reload;
+    return actor->needs_reload;
 }
 
 C_API bool actor_reload(Actor* actor)
@@ -143,7 +82,7 @@ C_API bool actor_reload(Actor* actor)
     if (!is_library(actor)) return true;
     if (!actor_needs_reload(actor)) return true;
 
-    FSFile* file = fs_volume_use_ref(actor->library.volume, actor->library.file);
+    FSFile* file = fs_volume_use_ref(actor->volume, actor->library);
     if (fs_file_needs_reload(file) && !fs_file_reload(file)) {
         log_error_if(actor->debug, "could not reload actor file");
         return false;
@@ -151,100 +90,34 @@ C_API bool actor_reload(Actor* actor)
     if (file->kind != FSFileMount_SystemMount) return true;
     int path_size = (int)file->system_mount.path.count;
     c_string path = file->system_mount.path.items;
-    void* handle = dlopen(path, RTLD_NOW);
+    void* handle = dlopen(path, RTLD_NOW|RTLD_LOCAL);
     if (!handle) {
         log_error_if(actor->debug, "could not open library '%.*s'", path_size, path);
         return false;
     }
 
-    c_string symbol = actor->library.dispatch_symbol;
-    void* (*dispatch)(ActorEvent) = dlsym(handle, symbol);
+    c_string symbol = actor->dispatch_name;
+    void (*dispatch)(void) = dlsym(handle, symbol);
     if (!dispatch) {
         log_error_if(actor->debug, "actor '%.*s' does not contain symbol '%s'", path_size, path, symbol);
         return false;
     }
-    usize new_size = size(dispatch);
-    if (new_size > actor_max_state_size) {
-        log_error_if(actor->debug, "actor size too big (size: %zu, max: %zu)", new_size, actor_max_state_size);
-        return false;
-    }
-    usize new_align = size(dispatch);
-    if (new_align > actor_max_state_align) {
-        log_error_if(actor->debug, "actor align too big (size: %zu, max: %zu)", new_align, actor_max_state_align);
-        return false;
-    }
 
-    usize old_size = size(actor->dispatch);
-    usize old_index = actor->state_index;
-    usize new_index = (old_index + 1) % actor_max_states;
-    if (actor->library.handles[new_index]) dlclose(actor->library.handles[new_index]);
-    actor->library.handles[new_index] = handle;
-    actor->state_index = new_index;
+    u64 old_index = actor->current_handle;
+    u64 new_index = (old_index + 1) % ty_array_size(actor->handles);
+    if (actor->handles[new_index]) dlclose(actor->handles[new_index]);
+    actor->handles[new_index] = handle;
+    actor->current_handle = new_index;
+    actor->needs_reload = false;
+
+    ty_write_barrier();
+    // __builtin___clear_cache((void*)dispatch, ((char*)dispatch) + 4096);
     actor->dispatch = dispatch;
-
-    u8* new_state = actor->state[new_index];
-    u8* old_state = actor->state[old_index];
-
-    memset(new_state, 0, new_size);
-    memcpy(new_state, old_state, old_size);
-    bind(dispatch, new_state);
-    actor->current_state = new_state;
-    actor->library.needs_reload = false;
 
     return true;
 }
 
-static usize size(void*(*dispatch)(ActorEvent))
-{
-    return (usize)dispatch((ActorEvent){
-        .self = 0,
-        .arg = 0,
-        .size = 0,
-        .tag = ActorEventTag_Size,
-    });
-}
-
-static usize align(void*(*dispatch)(ActorEvent))
-{
-    return (usize)dispatch((ActorEvent){
-        .self = 0,
-        .arg = 0,
-        .size = 0,
-        .tag = ActorEventTag_Align,
-    });
-}
-
-static bool init(void*(dispatch)(ActorEvent), u8* state, void const* arg, usize arg_size)
-{
-    return (bool)dispatch((ActorEvent){
-        .self = state,
-        .arg = arg,
-        .size = arg_size,
-        .tag = ActorEventTag_Init,
-    });
-}
-
-static void deinit(void*(dispatch)(ActorEvent), u8* state)
-{
-    dispatch((ActorEvent){
-        .self = state,
-        .arg = 0,
-        .size = 0,
-        .tag = ActorEventTag_Deinit,
-    });
-}
-
-static void bind(void*(*dispatch)(ActorEvent), u8* state)
-{
-    dispatch((ActorEvent){
-        .self = state,
-        .arg = 0,
-        .size = 0,
-        .tag = ActorEventTag_Bind,
-    });
-}
-
 static bool is_library(Actor const* actor)
 {
-    return actor->library.volume != 0;
+    return actor->volume != 0;
 }
--- Libraries/Library/Actor.h
+++ Libraries/Library/Actor.h
@@ -6,44 +6,19 @@
 
 #define actor_library_path(name) ty_library_path(name)
 
-typedef enum ActorEventTag {
-    ActorEventTag_Size,
-    ActorEventTag_Align,
-    ActorEventTag_Init,
-    ActorEventTag_Deinit,
-    ActorEventTag_Bind,
-} ActorEventTag;
-
-typedef struct ActorEvent {
-    void* self;
-    void const* arg;
-    usize size;
-    ActorEventTag tag;
-} ActorEvent;
-
-static constexpr u64 actor_max_states = 4;
-static constexpr u64 actor_max_state_size = 32;
-static constexpr u64 actor_max_state_align = 32;
-
 typedef struct Actor {
-    void* (*dispatch)(ActorEvent);
-    usize state_index;
     Logger* debug;
-    struct {
-        FSVolume* volume;
-        c_string dispatch_symbol;
-
-        FileID file;
-        bool needs_reload;
-        void* handles[actor_max_states];
-    } library;
-
-    u8* _Atomic current_state;
-    alignas(actor_max_state_align) u8 state[actor_max_states][actor_max_state_size];
+    FSVolume* volume;
+    void* handles[8];
+    c_string dispatch_name;
+    void (*_Atomic dispatch)(void);
+    u32 current_handle;
+    u32 needs_reload;
+    FileID library;
 } Actor;
 
-C_API bool actor_init(Logger* debug, void* (*dispatch)(ActorEvent), void const* arg, u64 arg_size, Actor*);
-C_API bool actor_init_reloadable(Logger* debug, FSVolume* volume, FileID library, c_string dispatch_name, void const* arg, u64 arg_size, Actor*);
+C_API Actor actor_init(void (*dispatch)(void));
+C_API bool actor_init_reloadable(Actor*, Logger* debug, FSVolume* volume, FileID library, c_string dispatch_name);
 C_API void actor_deinit(Actor*);
 
 C_API void actor_update(Actor*);

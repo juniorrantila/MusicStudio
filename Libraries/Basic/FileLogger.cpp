ec7bb5a3d4983f63171959bd0527f75c4680ad83
Junior Rantila <junior.rantila@gmail.com>
2026-01-02 16:55:04 +0100

    Basic: Use thread name in file logger as fallback for logger name

--- Libraries/Basic/FileLogger.cpp
+++ Libraries/Basic/FileLogger.cpp
@@ -2,12 +2,13 @@
 
 #include "./Logger.h"
 #include "./Types.h"
+#include "./Context.h"
 
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
 
-static void dispatch(struct Logger*, LoggerEvent);
+static void file_dispatch(struct Logger*, LoggerEvent);
 
 #define RED    "\033[1;31m"
 #define YELLOW "\033[1;33m"
@@ -18,14 +19,14 @@ static void dispatch(struct Logger*, LoggerEvent);
 C_API FileLogger file_logger_init(c_string name, FILE* file)
 {
     return (FileLogger){
-        .logger = logger_init(dispatch),
+        .logger = logger_init(file_dispatch),
         .file = file,
         .name = name,
         .is_tty = isatty(fileno(file)) == 1,
     };
 }
 
-static c_string severity(LoggerEventTag tag)
+static c_string file_severity(LoggerEventTag tag)
 {
     switch (tag) {
     case LoggerEventTag_Format: return "";
@@ -37,13 +38,13 @@ static c_string severity(LoggerEventTag tag)
     }
 }
 
-static c_string reset(bool is_tty)
+static c_string file_reset(bool is_tty)
 {
     if (!is_tty) return "";
     return NORMAL;
 }
 
-static c_string color(LoggerEventTag tag, bool is_tty)
+static c_string file_color(LoggerEventTag tag, bool is_tty)
 {
     if (!is_tty) return "";
     switch (tag) {
@@ -56,7 +57,7 @@ static c_string color(LoggerEventTag tag, bool is_tty)
     }
 }
 
-static void dispatch(struct Logger* l, LoggerEvent event)
+static void file_dispatch(struct Logger* l, LoggerEvent event)
 {
     FileLogger* file_logger = FIELD_BASE(FileLogger, logger, l);
     if (event.tag == LoggerEventTag_Format) {
@@ -65,7 +66,7 @@ static void dispatch(struct Logger* l, LoggerEvent event)
         return;
     }
 
-    if (event.tag == LoggerEventTag_Debug && file_logger->is_tty) {
+    if (event.tag == LoggerEventTag_Debug) {
         if (getenv("DEBUG") == nullptr)
             return;
     }
@@ -73,39 +74,33 @@ static void dispatch(struct Logger* l, LoggerEvent event)
     int len = (int)event.message_size;
     int pid = getpid();
 
+    c_string name = file_logger->name;
+    if (!name) name = context()->thread_name;
     bool is_tty = file_logger->is_tty;
-    if (file_logger->name) {
-        (void)fprintf(file_logger->file, "%s%s|%s%.2u%s|%s%u%s|%s%16s%s|%s %.*s\n",
-            color(event.tag, is_tty), // 1
-            severity(event.tag),
-            reset(is_tty),
-            event.seq % 100,
-            color(event.tag, is_tty),
-            reset(is_tty),
-            pid,
-            color(event.tag, is_tty),
-            reset(is_tty),
-            file_logger->name,
-            color(event.tag, is_tty),
-            reset(is_tty),
-            len,
-            event.message
-        );
-    } else {
-        (void)fprintf(file_logger->file, "%s%s|%s%.2u%s|%s%u%s%s: %.*s\n",
-            color(event.tag, is_tty), // 1
-            severity(event.tag),
-            reset(is_tty),
-            event.seq % 100,
-            color(event.tag, is_tty),
-            reset(is_tty),
-            pid,
-            color(event.tag, is_tty),
-            reset(is_tty),
-            len,
-            event.message
-        );
-    }
+    (void)fprintf(file_logger->file, "%s%s|%s%.2u%s|%s%u%s|%s%li%s|%s%16s%s|%s %.*s\n",
+        file_color(event.tag, is_tty), // 1
+        file_severity(event.tag),
+
+        file_reset(is_tty),
+        event.seq % 100,
+        file_color(event.tag, is_tty),
+
+        file_reset(is_tty),
+        pid,
+        file_color(event.tag, is_tty),
+
+        file_reset(is_tty),
+        kthread_index(event.originator),
+        file_color(event.tag, is_tty),
+
+        file_reset(is_tty),
+        name,
+        file_color(event.tag, is_tty),
+
+        file_reset(is_tty),
+        len,
+        event.message
+    );
 
     (void)fflush(file_logger->file);
 

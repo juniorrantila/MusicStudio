80f356a376738d3cea6763281ead5700e17688d5
Junior Rantila <junior.rantila@gmail.com>
2026-01-02 17:18:02 +0100

    LibMain: Initialize default context

--- Libraries/LibMain/Main.cpp
+++ Libraries/LibMain/Main.cpp
@@ -1,8 +1,22 @@
 #include "Main.h"
 #include <LibCore/File.h>
 
+#include <Basic/Context.h>
+
+#include <sys/syslimits.h>
+#include <stdlib.h>
+#include <libgen.h>
+
 int main(int argc, c_string argv[])
 {
+    static c_string program_name;
+    static char buf[PATH_MAX];
+    if (!program_name) {
+        basename_r(getenv("_"), buf);
+        program_name = buf;
+    }
+    init_default_context(program_name);
+
     auto result = Main::main(argc, argv);
     if (result.is_error()) {
         Core::File::stderr().writeln(result.error()).ignore();
